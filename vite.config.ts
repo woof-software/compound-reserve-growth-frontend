9ea1474cc3bd146e2491c25c922af1771636b51b
mmo0307 <mmo0307@gmail.com>
2025-06-17 19:28:11 +0300

    feat: add configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,35 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from 'path';
+import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), svgr({
-    include: /\.svg$/,
-    svgrOptions: {
-      icon: true,
-      svgoConfig: {
-        plugins: [
-          {
-            name: 'convertColors',
-            params: {
-              currentColor: true
+  plugins: [
+    react(),
+    tailwindcss(),
+    svgr({
+      include: /\.svg$/,
+      svgrOptions: {
+        icon: true,
+        svgoConfig: {
+          plugins: [
+            {
+              name: 'convertColors',
+              params: {
+                currentColor: true
+              }
             }
-          }
-        ]
+          ]
+        }
       }
-    }
-  }),],
+    })
+  ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+      '@': path.resolve(__dirname, './src')
+    }
   },
   assetsInclude: ['**/*.svg', '**/*.png', '**/*.jpg', '**/*.jpeg']
-})
+});
