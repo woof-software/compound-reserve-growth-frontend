4db1c46eaad09878df8850689efb13a95edcb5ff
mmo0307 <mmo0307@gmail.com>
2025-06-18 22:56:30 +0300

    feat: implement theme provider and update color system

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
+import { withTheme } from '@/shared/providers';
+
 function App() {
-  return <div>Reserve</div>;
+  return <div className='bg-bluish-purple text-hot-pink'>Reserve</div>;
 }
 
-export { App };
+export default withTheme(App);
