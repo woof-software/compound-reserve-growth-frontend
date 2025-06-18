dafc02803a686d525d6e37b0a0d10b0c9a8a17cc
mmo0307 <mmo0307@gmail.com>
2025-06-18 23:04:11 +0300

    refactor: update import paths for ThemeProvider and reorganize file structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
-import { AppRouter } from './app/providers/router/AppRouter';
-import Footer from './components/Footer/Footer';
-import Header from './components/Header/Header';
-import PageWrapper from './shared/ui/PageWrapper/PageWrapper';
-
-import { withTheme } from '@/shared/providers';
+import { AppRouter } from '@/app/providers/router/AppRouter';
+import { withTheme } from '@/app/providers/ThemeProvider';
+import Footer from '@/components/Footer/Footer';
+import Header from '@/components/Header/Header';
+import PageWrapper from '@/shared/ui/PageWrapper/PageWrapper';
 
 function App() {
   return (
     <PageWrapper className='gap-[60px]'>
       <Header />
+
       <main className='mx-auto flex w-full max-w-[1084px] flex-grow flex-col'>
         <AppRouter />
       </main>
+
       <Footer />
     </PageWrapper>
   );
