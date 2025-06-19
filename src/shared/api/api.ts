d9a66263069bdf7ff4b6d6c55ba91ecd7777df2e
mmo0307 <mmo0307@gmail.com>
2025-06-19 18:57:16 +0300

    feat: update api setting

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -28,70 +28,68 @@ export enum HttpVerbs {
   Put = 'PUT'
 }
 
-const httpRequest = async <T>(
+const httpRequest = async <T, P = unknown, D = unknown>(
   method: HttpVerbs,
   url: string,
-  paramsOrData?: any,
-  config?: AxiosRequestConfig
+  options?: {
+    params?: P;
+    data?: D;
+    config?: AxiosRequestConfig;
+  }
 ): Promise<ApiResponse<T, IApiError>> => {
+  const { params, data, config } = options ?? {};
   const requestConfig: AxiosRequestConfig = {
     ...config,
-
     url: `${import.meta.env.VITE_LOCAL_DOMAIN_NAME}${url}`,
-
     method,
-
-    params: method === HttpVerbs.Get ? paramsOrData : undefined,
-
-    data: method !== HttpVerbs.Get ? paramsOrData : undefined,
-
+    params: method === HttpVerbs.Get ? params : undefined,
+    data: method !== HttpVerbs.Get ? data : undefined,
     headers: {
       ...config?.headers,
-
       authorization: '',
-
       accept: 'application/json'
     }
   };
 
   try {
     const response: AxiosResponse<T> = await axios(requestConfig);
-
-    return {
-      data: response.data,
-
-      statusCode: response.status
-    };
+    return { data: response.data, statusCode: response.status };
   } catch (error: any) {
     if (axios.isAxiosError(error)) {
-      return {
-        data: undefined,
-
-        error
-      };
+      return { error, data: undefined };
     }
-
-    return {
-      data: undefined,
-
-      error: { message: 'Unexpected error occurred' }
-    };
+    return { error: { message: 'Unexpected error occurred' }, data: undefined };
   }
 };
 
 export const $api = {
-  get: async <T>(url: string, params?: any, config?: AxiosRequestConfig) =>
-    httpRequest<T>(HttpVerbs.Get, url, params, config),
+  get: async <T, P = unknown>(
+    url: string,
+    params?: P,
+    config?: AxiosRequestConfig
+  ) => httpRequest<T, P, never>(HttpVerbs.Get, url, { params, config }),
 
-  put: async <T>(url: string, data?: any, config?: AxiosRequestConfig) =>
-    httpRequest<T>(HttpVerbs.Put, url, data, config),
+  post: async <T, D = unknown>(
+    url: string,
+    data?: D,
+    config?: AxiosRequestConfig
+  ) => httpRequest<T, never, D>(HttpVerbs.Post, url, { data, config }),
 
-  patch: async <T>(url: string, data?: any, config?: AxiosRequestConfig) =>
-    httpRequest<T>(HttpVerbs.Patch, url, data, config),
+  put: async <T, D = unknown>(
+    url: string,
+    data?: D,
+    config?: AxiosRequestConfig
+  ) => httpRequest<T, never, D>(HttpVerbs.Put, url, { data, config }),
 
-  post: async <T>(url: string, data?: any, config?: AxiosRequestConfig) =>
-    httpRequest<T>(HttpVerbs.Post, url, data, config),
+  patch: async <T, D = unknown>(
+    url: string,
+    data?: D,
+    config?: AxiosRequestConfig
+  ) => httpRequest<T, never, D>(HttpVerbs.Patch, url, { data, config }),
 
-  delete: async <T>(url: string, config?: AxiosRequestConfig) =>
-    httpRequest<T>(HttpVerbs.Delete, url, null, config)
+  delete: async <T, P = unknown>(
+    url: string,
+    params?: P,
+    config?: AxiosRequestConfig
+  ) => httpRequest<T, P, never>(HttpVerbs.Delete, url, { params, config })
 };
