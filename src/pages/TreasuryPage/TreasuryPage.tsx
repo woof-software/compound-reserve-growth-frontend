125dbb29e75cf6e38a33b8a9ba35b839e066f5b3
mmo0307 <mmo0307@gmail.com>
2025-06-19 13:14:41 +0300

    feat: Icon Component

--- src/pages/TreasuryPage/TreasuryPage.tsx
+++ src/pages/TreasuryPage/TreasuryPage.tsx
@@ -1,6 +1,10 @@
+import FallbackImage from '@/shared/ui/FallbackImage/FallbackImage';
+
 const TreasuryPage = () => {
   return (
     <>
+      <FallbackImage src='svg/storage.svg' />
+
       <h1 className='mb-4 text-[32px] font-bold text-gray-900'>Treasury</h1>
       <p className='max-h-[300px] overflow-y-scroll'>
         Track Compound DAOs treasury portfolio including asset allocation,
