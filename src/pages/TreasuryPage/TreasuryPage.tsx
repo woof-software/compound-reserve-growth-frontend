efcf2ec7a2dc9955cfaf101962193f5f6feafd89
mmo0307 <mmo0307@gmail.com>
2025-06-24 16:03:01 +0300

    feat: Pie chart

--- src/pages/TreasuryPage/TreasuryPage.tsx
+++ src/pages/TreasuryPage/TreasuryPage.tsx
@@ -130,18 +130,6 @@ const TreasuryPage = () => {
                 { name: 'ETH Correlated', percent: 4.86, value: '0.54k' },
                 { name: 'DeFi', percent: 2.63, value: '0.23k' },
                 { name: 'BTC Correlated', percent: 2.6, value: '5.54k' },
-                { name: 'Unclassified', percent: 2.6, value: '0.54k' },
-                { name: 'AAVE', percent: 70.67, value: '1.54M' },
-                { name: 'Stablecoin', percent: 14.77, value: '1.54k' },
-                { name: 'ETH Correlated', percent: 4.86, value: '0.54k' },
-                { name: 'DeFi', percent: 2.63, value: '0.23k' },
-                { name: 'BTC Correlated', percent: 2.6, value: '5.54k' },
-                { name: 'Unclassified', percent: 2.6, value: '0.54k' },
-                { name: 'AAVE', percent: 70.67, value: '1.54M' },
-                { name: 'Stablecoin', percent: 14.77, value: '1.54k' },
-                { name: 'ETH Correlated', percent: 4.86, value: '0.54k' },
-                { name: 'DeFi', percent: 2.63, value: '0.23k' },
-                { name: 'BTC Correlated', percent: 2.6, value: '5.54k' },
                 { name: 'Unclassified', percent: 2.6, value: '0.54k' }
               ]}
             />
