ebe2d43120f75adcfee9d2d3c23c10be31d414e5
mmo0307 <mmo0307@gmail.com>
2025-08-18 18:00:52 +0300

    fix: font weight Treasury Composition

--- src/components/TreasuryPageTable/TreasuryComposition.tsx
+++ src/components/TreasuryPageTable/TreasuryComposition.tsx
@@ -61,7 +61,12 @@ const TreasuryComposition = ({
               className='h-6 w-6'
               folder={config.folder}
             />
-            <Text size='13'>{row.original.name}</Text>
+            <Text
+              size='13'
+              weight='500'
+            >
+              {row.original.name}
+            </Text>
           </div>
         )
       },
@@ -194,18 +199,20 @@ const TreasuryComposition = ({
           pageSize={10}
           enableSorting={true}
           headerCellClassName='py-[13px] px-[5px]'
-          cellClassName='py-3 px-[5px]'
-          headerTextClassName='text-primary-14 font-medium'
+          cellClassName='py-3 px-[5px] !font-medium'
+          headerTextClassName='text-primary-14'
         />
         <div className='flex items-center justify-between px-[5px] py-3'>
           <Text
             size='11'
+            weight='500'
             className='text-primary-14'
           >
             Total Balance
           </Text>
           <Text
             size='11'
+            weight='500'
             className='text-primary-14'
           >
             {formatPrice(totalBalance, 1)}
