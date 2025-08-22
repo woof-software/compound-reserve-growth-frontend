574b0df8b0aa300e93008375ae0e4c72fd0cfb9b
mmo0307 <mmo0307@gmail.com>
2025-08-22 23:41:01 +0300

    fix: The D/W/M & 7B/30B/90B/180B buttons height is 40px on Tablet/Mob view

--- src/shared/ui/TabsGroup/TabsGroup.tsx
+++ src/shared/ui/TabsGroup/TabsGroup.tsx
@@ -42,7 +42,7 @@ const TabsGroup = ({
     >
       <TabsList
         className={cn(
-          'h-10 w-fit rounded-full p-1 lg:h-8',
+          'h-8 w-fit rounded-full p-1',
           'bg-primary-18 dark:shadow-13 shadow-14 border-none',
           className?.list
         )}
@@ -52,18 +52,18 @@ const TabsGroup = ({
             key={tab}
             value={tab}
             className={cn(
-              'hover:bg-card-content flex cursor-pointer items-center justify-center rounded-full px-3 py-1 text-sm transition-opacity hover:opacity-70 md:text-[11px]',
-              'h-8 leading-6 lg:h-6',
+              'hover:bg-card-content flex cursor-pointer items-center justify-center rounded-full px-3 py-1 text-[11px] font-medium transition-opacity hover:opacity-70',
+              'h-6 leading-6',
               'text-primary-11',
               'data-[state=active]:bg-card-content',
               'data-[state=active]:text-primary-11',
-              'data-[state=active]:shadow-sm',
-              'data-[state=active]:!dark:shadow-13',
-              'data-[state=active]:!shadow-14',
               'border-none',
               className?.trigger,
               className?.activeTrigger &&
-                `data-[state=active]:${className?.activeTrigger}`
+                `data-[state=active]:${className?.activeTrigger}`,
+              {
+                '!shadow-13': value === tab
+              }
             )}
           >
             {tab}
