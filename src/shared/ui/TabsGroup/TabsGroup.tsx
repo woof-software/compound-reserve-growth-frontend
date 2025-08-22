a6702f061f1b7dd2b061cb4830205389a60b2695
mmo0307 <mmo0307@gmail.com>
2025-08-22 23:41:58 +0300

    fix: The D/W/M & 7B/30B/90B/180B buttons height is 40px on Tablet/Mob view

--- src/shared/ui/TabsGroup/TabsGroup.tsx
+++ src/shared/ui/TabsGroup/TabsGroup.tsx
@@ -1,4 +1,5 @@
 import { cn } from '@/shared/lib/classNames/classNames';
+import Each from '@/shared/ui/Each/Each';
 
 import { Tabs, TabsList, TabsTrigger } from '../Tabs/Tabs';
 
@@ -47,28 +48,31 @@ const TabsGroup = ({
           className?.list
         )}
       >
-        {tabs.map((tab) => (
-          <TabsTrigger
-            key={tab}
-            value={tab}
-            className={cn(
-              'hover:bg-card-content flex cursor-pointer items-center justify-center rounded-full px-3 py-1 text-[11px] font-medium transition-opacity hover:opacity-70',
-              'h-6 leading-6',
-              'text-primary-11',
-              'data-[state=active]:bg-card-content',
-              'data-[state=active]:text-primary-11',
-              'border-none',
-              className?.trigger,
-              className?.activeTrigger &&
-                `data-[state=active]:${className?.activeTrigger}`,
-              {
-                '!shadow-13': value === tab
-              }
-            )}
-          >
-            {tab}
-          </TabsTrigger>
-        ))}
+        <Each
+          data={tabs}
+          render={(tab) => (
+            <TabsTrigger
+              key={tab}
+              value={tab}
+              className={cn(
+                'hover:bg-card-content flex cursor-pointer items-center justify-center rounded-full px-3 py-1 text-[11px] font-medium transition-opacity hover:opacity-70',
+                'h-6 leading-6',
+                'text-primary-11',
+                'data-[state=active]:bg-card-content',
+                'data-[state=active]:text-primary-11',
+                'border-none',
+                className?.trigger,
+                className?.activeTrigger &&
+                  `data-[state=active]:${className?.activeTrigger}`,
+                {
+                  '!shadow-13': value === tab
+                }
+              )}
+            >
+              {tab}
+            </TabsTrigger>
+          )}
+        />
       </TabsList>
     </Tabs>
   );
