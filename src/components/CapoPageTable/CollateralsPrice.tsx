b20403324aa77f9d7d4c259cbc7937e86c8857ef
mmo0307 <mmo0307@gmail.com>
2025-09-08 17:00:03 +0300

    fix: The Price feed values are restricted differently from the Figma

--- src/components/CapoPageTable/CollateralsPrice.tsx
+++ src/components/CapoPageTable/CollateralsPrice.tsx
@@ -1,14 +1,20 @@
 import * as React from 'react';
 import { useMemo } from 'react';
 
+import HoverCard from '@/components/HoverCard/HoverCard';
 import { MobileDataTable } from '@/components/MobileDataTable/MobileDataTable';
 import { cn } from '@/shared/lib/classNames/classNames';
-import { formatLargeNumber, formatPrice } from '@/shared/lib/utils/utils';
+import {
+  defaultExplorer,
+  explorers,
+  formatLargeNumber,
+  formatPrice,
+  sliceAddress
+} from '@/shared/lib/utils/utils';
+import { ClipboardButton } from '@/shared/ui/AnimationProvider/CopyButton/CopyButton';
 import DataTable, { ExtendedColumnDef } from '@/shared/ui/DataTable/DataTable';
 import Icon from '@/shared/ui/Icon/Icon';
 import Text from '@/shared/ui/Text/Text';
-
-import { AddressTooltip } from '../AddressTooltip/AddressTooltip';
 
 export type TreasuryBalanceByNetworkType = {
   symbol: string;
@@ -125,13 +131,62 @@ const treasuryColumns: ExtendedColumnDef<TreasuryBalanceByNetworkType>[] = [
     header: 'Price Feed',
     enableSorting: true,
     size: 120,
-    cell: ({ row }) => (
-      <AddressTooltip
-        text={row.original.address}
-        address={row.original.address}
-        chain={row.original.chain}
-      />
-    )
+    cell: ({ row }) => {
+      const explorerUrl =
+        (row.original.chain && explorers[row.original.chain.toLowerCase()]) ||
+        defaultExplorer;
+
+      const fullExplorerLink = `${explorerUrl}${row.original.address}`;
+      return (
+        <HoverCard
+          content={
+            <div className='flex w-50 flex-col items-start gap-2'>
+              <div className='flex w-full items-center justify-between'>
+                <Text
+                  size='12'
+                  className='text-primary-11'
+                >
+                  {sliceAddress(row.original.address, 7)}
+                </Text>
+                <ClipboardButton textToCopy={row.original.address} />
+              </div>
+              <div className='flex w-full items-center justify-between'>
+                <Text
+                  size='12'
+                  className='text-primary-11'
+                >
+                  View on Explorer
+                </Text>
+                <a
+                  href={fullExplorerLink}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  className='text-primary-11 flex h-4 w-4 items-center justify-center'
+                >
+                  <Icon
+                    name={'arrow-link'}
+                    className='h-4.5 w-3 text-[#7A8A99]'
+                  />
+                </a>
+              </div>
+            </div>
+          }
+          side='top'
+        >
+          <div
+            className='flex items-start'
+            style={{ width: `${120}px` }}
+          >
+            <Text
+              size='13'
+              className='text-primary-11 inline-block max-w-full cursor-pointer truncate border-b border-dotted border-gray-500 leading-none'
+            >
+              {`${row.original.address.slice(0, 4)}...${row.original.address.slice(-4)}`}
+            </Text>
+          </div>
+        </HoverCard>
+      );
+    }
   }
 ];
 
