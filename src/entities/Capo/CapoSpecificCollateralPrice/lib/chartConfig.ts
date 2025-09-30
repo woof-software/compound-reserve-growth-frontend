83321ff22e087a67922b7adbf2c42bd54cdfc7b7
Nikita <nikita.s@woof.software>
2025-09-30 16:14:50 +0300

    fix: chart config styles

--- src/entities/Capo/CapoSpecificCollateralPrice/lib/chartConfig.ts
+++ src/entities/Capo/CapoSpecificCollateralPrice/lib/chartConfig.ts
@@ -3,18 +3,23 @@ import Highcharts from 'highcharts';
 export const customFormatter = (context: { x: number; points: any[] }) => {
   const header = `<div style="font-weight: 500; margin-bottom: 12px; font-size: 11px; font-family: 'Haas Grot Text R', sans-serif;">${Highcharts.dateFormat('%B %e, %Y', context.x)}</div>`;
 
-  const body = context.points
-    ?.map(
-      (point) =>
-        `<div style="display: flex; justify-content: space-between; align-items: center; gap: 16px;">
+  const body = `<div style='display: flex; flex-direction: column; gap: 12px'>
+  ${
+    context.points
+      ?.reverse()
+      .map(
+        (point) =>
+          `<div style="display: flex; justify-content: space-between; align-items: center; gap: 16px;">
           <div style="display: flex; align-items: center; gap: 8px;">
             <span style="background-color:${point.series.color}; width: 10px; height: 10px; display: inline-block; border-radius: 2px;"></span>
             <span style="font-size: 11px; font-family: 'Haas Grot Text R', sans-serif;">${point.series.name}</span>
           </div>
           <span style="font-weight: 400; font-size: 11px; font-family: 'Haas Grot Text R', sans-serif;">$${Number(point.y).toFixed(2)}</span>
         </div>`
-    )
-    .join('');
+      )
+      .join('') || ''
+  }
+</div>`;
 
   return header + body;
 };
