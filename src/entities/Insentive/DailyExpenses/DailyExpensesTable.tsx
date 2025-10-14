ba5b9641eb7a50fbbd28ef7ed0f23d3bcaed76f8
Nikita <nikita.s@woof.software>
2025-10-14 13:54:58 +0300

    fix: token names

--- src/entities/Insentive/DailyExpenses/DailyExpensesTable.tsx
+++ src/entities/Insentive/DailyExpenses/DailyExpensesTable.tsx
@@ -79,7 +79,7 @@ const getDailyColumns = (
         <Text size='13'>
           {view === 'USD'
             ? Format.price(row.original.lendIncentive, 'standard')
-            : Format.token(row.original.lendIncentive, 'standard')}
+            : Format.token(row.original.lendIncentive, 'standard', 'COMP')}
         </Text>
       )
     },
@@ -93,7 +93,7 @@ const getDailyColumns = (
         <Text size='13'>
           {view === 'USD'
             ? Format.price(row.original.borrowIncentive, 'standard')
-            : Format.token(row.original.borrowIncentive, 'standard')}
+            : Format.token(row.original.borrowIncentive, 'standard', 'COMP')}
         </Text>
       )
     },
@@ -110,7 +110,7 @@ const getDailyColumns = (
         >
           {view === 'USD'
             ? Format.price(row.original.total, 'standard')
-            : Format.token(row.original.total, 'standard')}
+            : Format.token(row.original.total, 'standard', 'COMP')}
         </Text>
       )
     }
@@ -231,7 +231,7 @@ const DailyExpensesTable = (props: DailyExpensesTableProps) => {
                   >
                     {activeViewTab === 'USD'
                       ? Format.price(row.lendIncentive, 'standard')
-                      : Format.token(row.lendIncentive, 'standard')}
+                      : Format.token(row.lendIncentive, 'standard', 'COMP')}
                   </Text>
                 </div>
                 <div className='grid w-full'>
@@ -250,7 +250,7 @@ const DailyExpensesTable = (props: DailyExpensesTableProps) => {
                   >
                     {activeViewTab === 'USD'
                       ? Format.price(row.borrowIncentive, 'standard')
-                      : Format.token(row.borrowIncentive, 'standard')}
+                      : Format.token(row.borrowIncentive, 'standard', 'COMP')}
                   </Text>
                 </div>
                 <div className='grid w-full'>
@@ -269,7 +269,7 @@ const DailyExpensesTable = (props: DailyExpensesTableProps) => {
                   >
                     {activeViewTab === 'USD'
                       ? Format.price(row.total, 'standard')
-                      : Format.token(row.total, 'standard')}
+                      : Format.token(row.total, 'standard', 'COMP')}
                   </Text>
                 </div>
               </div>
