fb916b44417c8380da879c3ba61bbb300e6b756b
Stanislav Baraniuk <stas.b@woof.software>
2025-10-16 18:51:17 +0200

    fix: fix revenue page crash - reason getSummarizedCsvData - wrong types

--- src/shared/lib/utils/getSummarizedCsvData.ts
+++ src/shared/lib/utils/getSummarizedCsvData.ts
@@ -1,34 +1,35 @@
 import { SeriesAreaOptions, SeriesColumnOptions } from 'highcharts';
 
-interface CsvRow {
-  Date: string;
-  [networkId: string]: string | number;
-}
-
-export const getSummarizedCsvData = <
-  T extends SeriesAreaOptions | SeriesColumnOptions
->(
-  aggregatedSeries: T[]
-): CsvRow[] => {
-  const dateMap = new Map<string, CsvRow>();
+export function getSummarizedCsvData(
+  aggregatedSeries: Array<SeriesAreaOptions | SeriesColumnOptions>
+): Record<string, string>[] {
+  const dateMap = new Map<string, Record<string, string>>();
 
   aggregatedSeries.forEach((network) => {
-    if (network.data && network.id) {
-      // @ts-expect-error TODO: fix timestamp and value types
-      network.data.forEach(([timestamp, value]) => {
-        const date = new Date(timestamp).toISOString().split('T')[0];
+    const { data, id: chain } = network;
 
-        if (!dateMap.has(date)) {
-          dateMap.set(date, { Date: date });
-        }
+    if (!data || !chain) return;
 
-        const entry = dateMap.get(date)!;
-        entry[network.id!] = value;
-      });
-    }
+    data.forEach((record) => {
+      if (!Array.isArray(record)) return;
+
+      const [timestamp, value] = record;
+
+      if (typeof timestamp !== 'number') return;
+
+      const date = new Date(timestamp).toISOString().split('T')[0];
+
+      if (!dateMap.has(date)) {
+        dateMap.set(date, { Date: date });
+      }
+
+      const entry = dateMap.get(date)!;
+
+      entry[chain] = `${value}`;
+    });
   });
 
   return Array.from(dateMap.values()).sort((a, b) =>
     a.Date.localeCompare(b.Date)
   );
-};
+}
