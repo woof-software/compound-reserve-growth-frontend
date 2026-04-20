0bfba4616d40d3ee7a5217e91251c8d0177631b6
Nikita <nikita.s@woof.software>
2026-04-20 15:35:24 +0300

    refactor: remove unused libs

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,4 @@
 // eslint.config.mjs
-import dotenv from 'dotenv';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-
-dotenv.config();
-
 import { defineConfig, globalIgnores } from 'eslint/config';
 import importPlugin from 'eslint-plugin-import';
 import pluginChecker from 'eslint-plugin-plugin-checker';
@@ -13,6 +7,8 @@ import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import simpleImportSort from 'eslint-plugin-simple-import-sort';
 import unusedImports from 'eslint-plugin-unused-imports';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 import { fixupConfigRules, fixupPluginRules } from '@eslint/compat';
 import { FlatCompat } from '@eslint/eslintrc';
 import js from '@eslint/js';
